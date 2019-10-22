1d1f95b5f8dbdc58e33fb0a47cc290348daf9ec1
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-22 00:36:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,6 @@
 class School
   def initialize(school_name)
-    @school_name = school_name
+    @school = school_name
     @roster = {}
   end
   
@@ -8,28 +8,16 @@ class School
     @roster
   end
   
-  def add_student(name, grade)
-    @student_name = name
-    @student_grade = grade
-    if !@roster.include?(grade)
-      @roster[grade] = []
+  def add_student(student_name, student_grade)
+    @student_name = student_name
+    @student_grade = student_grade
+    if !@roster.include?(student_grade)
+      @roster[student_grade] = []
     end
-    @roster[grade] << name
+    @roster[student_grade] << student_name
   end
   
   def grade(number)
-    @roster.each do |grade, student| 
-      if number == grade
-        return student
-      end
-    end
-  end
-  
-  def sort
-    new = {}
-    roster.each do |x, y|
-      new[x] = y.sort
-    end
-    new
+    @roster.each do {|grade, student| (number == grade ? return student : nil)}end
   end
 end
